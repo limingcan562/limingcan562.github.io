87e01c81fab2d8c5a6bea2177c7ebc28bf1f1dce
lMC <804666207@qq.com>
2021-05-24 19:09:19 +0800

    init

---
+++ deploy.js
@@ -0,0 +1,20 @@
+const ghpages = require('gh-pages');
+const colors = require('colors');
+const onlineUrl = 'https://limingcan562.github.io/'
+const txtStart = 'dist deploying...';
+const txtEnd = 'dist successfully deployed!';
+const txtView = 'preview in';
+
+
+console.log('[deploy]'.yellow, txtStart.green);
+ghpages.publish('./dist', {
+    branch: 'master',
+    repo: 'https://github.com/limingcan562/limingcan562.github.io.git'
+}, (err) => {
+    if (err) {
+        console.log('[error]'.red, JSON.stringify(err).red);
+    } else {
+        console.log('[deploy]'.yellow, txtEnd.green);
+        console.log('[deploy]'.yellow, `${txtView} ${onlineUrl.green}`);
+    }
+});
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main_container">
+    这是一个正常部署的页面222
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>

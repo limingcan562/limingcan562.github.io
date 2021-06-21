b5f98eff0ecb68adabdeeef0d0f9c61e3d253e50
lMC <804666207@qq.com>
2021-06-21 20:23:52 +0800

    deploy

--- _nuxt/static/1624273012/posts/webpack-pack-path/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624273012",layout:"posts",error:a,state:{previewPicSrc:"",showPreviewPop:0,canScroll:1},serverRendered:true,routePath:"\u002Fposts\u002Fwebpack-pack-path",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1624278225/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fposts\u002Fcreate-blog-github","\u002Fposts\u002Fwebpack-pack-path"]})

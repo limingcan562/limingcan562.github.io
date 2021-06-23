60cc39a274f8e394c72782346936b8b21a74cd6b
lMC <804666207@qq.com>
2021-06-23 17:44:46 +0800

    deploy

--- _nuxt/static/1624432307/posts/webpack-pack-path/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624432307",layout:"posts",error:a,state:{previewPicSrc:"",showPreviewPop:b,canScroll:1,viewClickTip:b},serverRendered:true,routePath:"\u002Fposts\u002Fwebpack-pack-path",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,0));

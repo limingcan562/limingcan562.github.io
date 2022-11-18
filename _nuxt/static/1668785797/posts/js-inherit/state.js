5b6b210637e64b4dc93addb06529bc2e9514a42e
limingcan-home <804666207@qq.com>
2022-11-18 23:40:48 +0800

    deploy

--- _nuxt/static/1668785797/posts/js-inherit/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668785797",layout:"posts",error:a,state:{previewPicSrc:"",showPreviewPop:b,canScroll:1,viewClickTip:b},serverRendered:true,routePath:"\u002Fposts\u002Fjs-inherit",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,0));
---
+++ _nuxt/static/1668786034/posts/let-miniprogram-use-less/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668786034",layout:"posts",error:a,state:{previewPicSrc:"",showPreviewPop:b,canScroll:1,viewClickTip:b},serverRendered:true,routePath:"\u002Fposts\u002Flet-miniprogram-use-less",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,0));

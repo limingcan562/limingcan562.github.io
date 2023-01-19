ade09f1b29297ca5b216f5c18f91aa141bbd6ca5
lMC <804666207@qq.com>
2023-01-19 14:23:36 +0800

    deploy

---
+++ _nuxt/static/1674109408/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674109408",layout:"default",error:a,state:{previewPicSrc:"",showPreviewPop:b,canScroll:1,viewClickTip:b},serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,0));

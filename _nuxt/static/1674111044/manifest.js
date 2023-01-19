6c4a9df99ffc56c252e473196ee82cea8860207c
lMC <804666207@qq.com>
2023-01-19 17:21:04 +0800

    deploy

--- _nuxt/static/1674111044/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fposts\u002Fjs-inherit","\u002Fposts\u002Fknow-babel","\u002Fposts\u002Fwebpack-pack-path","\u002Fposts\u002Flet-miniprogram-use-less","\u002Fposts\u002Fabout-prototype","\u002Fposts\u002Fbuild-webpack5-feeling","\u002Fposts\u002Fcreate-blog-github"]})

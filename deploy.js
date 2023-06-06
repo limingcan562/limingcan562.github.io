4cb267d68d1cb29ed005eb7af8b76bc1a47d9620
lMC <804666207@qq.com>
2023-06-06 14:25:43 +0800

    修复发布模式

--- deploy.js
+++ deploy.js
@@ -9,7 +9,7 @@ const txtView = 'preview in';
 console.log('[deploy]'.bold.yellow, txtStart.bold.yellow);
 ghpages.publish('./dist', {
     branch: 'gh-pages',
-    repo: 'git@github.com:limingcan562/limingcan562.github.io.git',
+    repo: 'https://github.com/limingcan562/limingcan562.github.io.git',
     dotfiles: true,
     message: 'deploy'
 }, err => {
